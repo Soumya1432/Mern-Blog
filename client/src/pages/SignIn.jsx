76bc8e2b2fa72bc31ce76d46d2e28ca8b75ad80d
Soumya1432 <biswassoumya17@gmail.com>
2024-07-28 01:06:33 +0530

    change

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -49,12 +49,9 @@ export default function SignIn() {
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-<<<<<<< HEAD
              mern
-=======
               mern
                 
->>>>>>> a29a94f54f34fab563b46f1e0ed176cff7c28baf
             </span>
             Blog
           </Link>
